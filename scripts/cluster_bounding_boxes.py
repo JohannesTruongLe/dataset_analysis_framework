3649f2f833bc82ee32ef0b35fa42fc11aa37991a
JohannesTruongLe <johannes.truongle@gmail.com>
2019-05-25 14:58:33 +0200

    Add TSNE script

---
+++ scripts/cluster_bounding_boxes.py
@@ -0,0 +1,64 @@
+# TODO DOCSTRING ME
+
+from pathlib import Path
+
+import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import pandas as pd
+
+# TODO Config
+# TODO Argparse
+
+N_SAMPLES = 1500
+PATH_TO_FEATURES = Path('C:/workspace/data/meta/box_features')
+PATH_TO_LABELS = Path('C:/workspace/data/meta/data.pickle')
+CLASSES = 'Car Van Truck Pedestrian Person_sitting Cyclist Tram'.split()
+COLORS = 'b g r c m y'.split()
+
+CLASS_COLOR_DICT = {
+    'Car': 'b',
+    'Van': 'g',
+    'Truck': 'r',
+    'Pedestrian': 'c',
+    'Person_sitting': 'm',
+    'Cyclist': 'm',
+    'Tram': 'y'
+}
+
+
+def _load_data_from_directory(path, n_samples=None):
+
+    if not n_samples:
+        n_samples = -1
+
+    data = []
+    for path_to_data in tqdm.tqdm(list(path.glob('*'))[:n_samples]):
+        data.append(np.load(path_to_data))
+
+    return data
+
+
+def _plot(type_container, embedded_space, class_color_dict):
+
+    for type, color in class_color_dict.items():
+        subset = embedded_space[type_container == type, :]
+        plt.scatter(subset[:, 0], subset[:, 1], c=color, label=type)
+        plt.legend()
+    plt.show()
+
+
+def _main():
+
+    type_container = pd.read_pickle(str(PATH_TO_LABELS))['type'][:N_SAMPLES]
+    data = _load_data_from_directory(PATH_TO_FEATURES, n_samples=N_SAMPLES)
+    embedded_space = TSNE(verbose=5).fit_transform(data)
+
+    _plot(type_container=type_container,
+          embedded_space=embedded_space,
+          class_color_dict=CLASS_COLOR_DICT)
+
+
+if __name__ == '__main__':
+    _main()
